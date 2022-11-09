e7dcc58aa3c139d14b80dbc6593bff37beb2ca8d
Kepledo <edoardomelisinfo@gmail.com>
2022-11-09 13:34:47 +0100

    master

--- src/test/java/com/example/provapluginbranch/ProvaPluginBranchApplicationTests.java
+++ src/test/java/com/example/provapluginbranch/ProvaPluginBranchApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.provapluginbranch;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,19 +14,19 @@ class ProvaPluginBranchApplicationTests {
     @Test
     void addizione() {
         Integer totale = calcolo.calcoloAddizione();
-        Assertions.assertEquals(4, 4);
+        Assertions.assertEquals(4, totale);
     }
 
     @Test
     void sottrazione() {
         Integer totale = calcolo.calcoloSottrazione();
-        Assertions.assertEquals(1, 1);
+        Assertions.assertEquals(1, totale);
     }
 
     @Test
     void moltiplicazione() {
         Integer totale = calcolo.calcoloMoltiplicazione();
-        Assertions.assertEquals(4, 4);
+        Assertions.assertEquals(4, totale);
     }
 
 }
