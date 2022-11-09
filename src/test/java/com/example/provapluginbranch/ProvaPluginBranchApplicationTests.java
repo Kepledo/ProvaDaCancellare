bb857fe1aa2887a8d17746dd735986fc169f348c
Kepledo <edoardomelisinfo@gmail.com>
2022-11-09 13:15:57 +0100

    Primo commit develop

--- src/test/java/com/example/provapluginbranch/ProvaPluginBranchApplicationTests.java
+++ src/test/java/com/example/provapluginbranch/ProvaPluginBranchApplicationTests.java
@@ -12,22 +12,6 @@ class ProvaPluginBranchApplicationTests {
     @Autowired
     Calcolo calcolo;
 
-    @Test
-    void addizione() {
-        Integer totale = calcolo.calcoloAddizione();
-        Assertions.assertEquals(4, 4);
-    }
 
-    @Test
-    void sottrazione() {
-        Integer totale = calcolo.calcoloAddizione();
-        Assertions.assertEquals(1, 1);
-    }
-
-    @Test
-    void moltiplicazione() {
-        Integer totale = calcolo.calcoloAddizione();
-        Assertions.assertEquals(4, 4);
-    }
 
 }
